ec83386779c9dd93f3ef314e21574d3dd3004907
Kot <umaduy@yandex.ru>
2024-02-19 11:07:02 +0300

    Вывод строки.

--- V_01/program.java
+++ V_01/program.java
@@ -3,14 +3,14 @@ package V_01;
 public class program {
   
     public static void main(String[] args) {
-     
-      
+           
         System.out.println("Привет Вячеслав!");
         System.out.println("Hello Vyacheslav!");
+        System.out.print("Здравствуйте Вячеслав! \n");
         System.out.printf("%5S \n", "Vyacheslav!");
         System.out.printf("%.5S \n", "Vyacheslav!");
         System.out.printf("%.5s ", "Vyacheslav");
-        System.out.printf("%5s", "Vladimirovich!");
+        System.out.printf("%.6s", "Vladimirovich!");
 
     }         
 
