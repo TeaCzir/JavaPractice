baed4a88fc88a08e3c30bd49aac3e9c67483b320
Kot <umaduy@yandex.ru>
2024-02-19 10:45:04 +0300

    Initialization.

---
+++ V_01/program.java
@@ -0,0 +1,17 @@
+package V_01;
+
+public class program {
+  
+    public static void main(String[] args) {
+     
+      
+        System.out.println("Привет Вячеслав!");
+        System.out.println("Hello Vyacheslav!");
+        System.out.printf("%5S \n", "Vyacheslav!");
+        System.out.printf("%.5S \n", "Vyacheslav!");
+        System.out.printf("%.5s ", "Vyacheslav");
+        System.out.printf("%5s", "Vladimirovich!");
+
+    }         
+
+}
